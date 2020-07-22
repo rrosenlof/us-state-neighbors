9ec1b69c15b222e89fde4ecdb0477f365113b6a2
ross <rosenlof.ross@gmail.com>
2020-07-22 00:28:54 -0600

    getting closer...

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ states = []
 #                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 #     border_writer.writerows(states)
 
+
+
+
 def main():
     border_paths = []
-    border_dict = create_dict()
+
+    border_dict = create_dict()     
 
     with open('borders.csv', newline='') as csvfile:
         borders = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -35,12 +39,14 @@ def main():
                 if state1 == state2:
                     border_paths.append([state1,state2,dist])
                 else:
-                    dist = find_borders(state1,state2, border_dict)
-                    border_paths.append([state1,state2,dist])
+                    for obj in find_borders(state1,state2,border_dict):
+                        print(obj)
+                        border_paths.append(obj)
+                    # border_paths.append(res)
                 j += 1
             i += 1
         
-    print(border_paths)
+    # print(border_paths)
 
 def create_dict():
     with open('borders.csv', newline='') as csvfile:
@@ -50,23 +56,16 @@ def create_dict():
             mydict = {rows[1]:rows[2] for rows in reader}
             return mydict
 
-def find_borders(state1, state2, border_dict, dist=0, index=0):
-    # print(borders)
-    # borders = borders.split('/')
-    borders = border_dict[state1]
-    borders = borders.split('/')
-    dist += 1
-    if state2 == borders[index]:
-        print(state2,borders[index])
-        return dist
-    index += 1
-    if index > len(borders):
-        print(borders[0])
-        return find_borders(state1,state2,border_dict[borders[0]],dist)
-    return find_borders(state1,state2,borders,dist,index)
-    
-    
-    
+def find_borders(state1, state2, border_dict, dist=1,index=0):
+    print('...................')
+    print('state1: {}'.format(state1))
+    print('index: {}'.format(index))
+    for border in border_dict[state1].split('/'):
+        print(border)
+        if border == state2:
+            yield ([state1, state2, dist])
+    yield from find_borders(border_dict[state1].split('/')[index],state2,border_dict,dist+1,index+1)
+      
 
 main()
     
